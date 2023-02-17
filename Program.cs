2c40e237897175cf0f4381984cd3252d71a6b3af
joao.bruno <joao.bruno@ciatecnica.com.br>
2023-02-17 08:28:25 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite um texto:");
+
+var Texto = Console.ReadLine();
+
+int NumCaracteres = Texto.Length;
+int NumPalavras = NumCaracteres > 0 ? Texto.Split(" ").Length : 0;
+
+Console.WriteLine($"{NumCaracteres} caracteres, {NumPalavras} palavras");
+Console.ReadKey();
